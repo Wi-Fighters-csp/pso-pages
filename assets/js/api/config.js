e8606d953db37291ad528483c44f788dba25f138
jm1021 <jmort1021@gmail.com>
2026-04-01 10:31:00 -0700

    change python uri

--- assets/js/api/config.js
+++ assets/js/api/config.js
@@ -9,7 +9,7 @@ export var pythonURI;
 if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
     pythonURI = "http://localhost:8587";  // Same URI for localhost or 127.0.0.1
 } else {
-    pythonURI = "https://flask.opencodingsociety.com";
+    pythonURI = "https://wifighters.opencodingsociety.com";
 
 }
 
